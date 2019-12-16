68f50645c6c99e96e714216a36d6a7a602b7b24e
RenissonSilva <renissonx@gmail.com>
2019-12-16 20:06:06 -0300

    Adicionando as functions no controller e mensagens de resposta

--- casa_inteligente/app/Http/Controllers/Api/SensorController.php
+++ casa_inteligente/app/Http/Controllers/Api/SensorController.php
@@ -8,12 +8,62 @@ use App\Sensor;
 
 class SensorController extends Controller
 {
-    public function index()
+	private $sensors;
+
+	public function __construct(Sensor $sensors)
+	{
+		$this->sensors = $sensors;
+	}
+	public function index()
     {
-    	return Sensor::all();
+    	return response()->json($this->sensors->all());
+    }
+    public function show($id)
+    {
+    	$sensors = $this->sensors->find($id);
+    	if(! $sensors) return response()->json(ApiError::errorMessage('Sensor não encontrado!', 4040), 404);
+    	$data = ['data' => $sensors];
+	    return response()->json($data);
     }
     public function store(Request $request)
     {
-        Sensor::create($request->all());
+		try {
+			$sensorsData = $request->all();
+			$this->sensors->create($sensorsData);
+			$return = ['data' => ['msg' => 'Sensor criado com sucesso!']];
+			return response()->json($return, 201);
+		} catch (\Exception $e) {
+			if(config('app.debug')) {
+				return response()->json(ApiError::errorMessage($e->getMessage(), 1010), 500);
+			}
+			return response()->json(ApiError::errorMessage('Houve um erro ao realizar operação de salvar', 1010),  500);
+		}
     }
+	public function update(Request $request, $id)
+	{
+		try {
+			$sensorsData = $request->all();
+			$sensors     = $this->sensors->find($id);
+			$sensors->update($sensorsData);
+			$return = ['data' => ['msg' => 'Sensor atualizado com sucesso!']];
+			return response()->json($return, 201);
+		} catch (\Exception $e) {
+			if(config('app.debug')) {
+				return response()->json(ApiError::errorMessage($e->getMessage(), 1011),  500);
+			}
+			return response()->json(ApiError::errorMessage('Houve um erro ao realizar operação de atualizar', 1011), 500);
+		}
+	}
+	public function delete(Sensor $id)
+	{
+		try {
+			$id->delete();
+			return response()->json(['data' => ['msg' => 'Produto: ' . $id->name . ' removido com sucesso!']], 200);
+		}catch (\Exception $e) {
+			if(config('app.debug')) {
+				return response()->json(ApiError::errorMessage($e->getMessage(), 1012),  500);
+			}
+			return response()->json(ApiError::errorMessage('Houve um erro ao realizar operação de remover', 1012),  500);
+		}
+	}
 }
