3ce20b0940ff99eb98e418efd90afa00b29ba837
GuiLira <liraguilherme252@gmail.com>
2019-12-18 00:25:13 -0300

    GH-2 Modificações em show.

--- casa_inteligente/app/Http/Controllers/Api/MedicaoController.php
+++ casa_inteligente/app/Http/Controllers/Api/MedicaoController.php
@@ -17,25 +17,15 @@ class MedicaoController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    public function __construct(Sensor $sensors)
-	{
-		$this->sensors = $sensors;
-	}
-
-
-    public function __constructM(Medicoe $medicoes)
+    private $medicoe;
+    public function __construct(Medicoe $medicoes)
     {
         $this->medicoe = $medicoes;
     }
 
     public function index()
     {
-        if(sizeof($this->sensors->all()) <= 0 ){
-            return response()->json(['data' => ['msg' => 'Não existe nenhuma medição salva']], 404);
-        }
-        else{
-            return response()->json($this->sensors->all());
-        }
+
     }
 
     /**
@@ -57,27 +47,27 @@ class MedicaoController extends Controller
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'sensor_id' => 'required|integer|min:1|max:3',
-            'valor' => 'in:celcius|required|min:7|max:7',
+            'sensor_id' => 'required|numeric|',
+            'valor' => 'required|numeric|',
             'data_horario' => 'required|date',
         ]);
         if(sizeof($validator->errors()) > 0 ){
             return response()->json($validator->errors(), 404);
         }
 
-        try{
+        //try{
             $medicaoData = $request->all();
             $this->medicoe->create($medicaoData);
             $data = Medicoe::where('sensor_id', $request->get('sensor_id'))->get();
-            $return = ['data' => ['msg' => 'Medição criado com sucesso!']];
+            $return = ['data' => ['msg' => ['Medição criado com sucesso!'], [$data]]];
             return response()->json($return, 201);
 
-        }catch(\Exception $e){
-            if(is_null($request->sensor_id)) return response()->json(['data'=>['msg'=>'Sensor não encontrado!']], 404);
-        }
+        //}catch(\Exception $e){
+            //if(is_null($request->sensor_id)) return response()->json(['data'=>['msg'=>'Sensor não encontrado!']], 404);
+        //}
 
-        //$req = $request->all();
-        //return response()->json($req);
+        $req = $request->all();
+        //return response()->json($return);
     }
 
     /**
@@ -86,10 +76,18 @@ class MedicaoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show()
+    public function show($id)
     {
-        $medicoes = Medicoe::where('sensor_id')->get();
-        return view('indexMedicoes', compact('medicoes'));
+        $medicoes = Medicoe::where('id', $id)->get();
+        $teste = count($medicoes);
+        if($teste != 0){
+            $dataMedi = ['data' => [$medicoes]];
+            return response()->json($dataMedi ,201);
+        }else{
+            $empty = ['data' => ["Não tem medições com esse id"]];
+            return response()->json($empty, 404);
+        }
+
     }
 
     /**
