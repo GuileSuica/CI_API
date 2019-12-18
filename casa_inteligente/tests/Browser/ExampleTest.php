3908cd296e6138d5aff6e54d852ee40ef7d3752c
RenissonSilva <renissonx@gmail.com>
2019-12-18 10:44:37 -0300

    GH-5 Criação da base dos teste

---
+++ casa_inteligente/tests/Browser/ExampleTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $response = $this->get('/api/sensor/');
+
+        $response->assertStatus(200);
+    }
+}
